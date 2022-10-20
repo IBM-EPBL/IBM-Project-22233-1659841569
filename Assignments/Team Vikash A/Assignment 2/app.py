4c5d731db869f8242548a7a3b29a14d33670004d
Vikash A <104715518+vikashkec@users.noreply.github.com>
2022-10-20 11:19:32 +0530

    Update app.py

--- Assignments/Team Vikash A/Assignment 2/app.py
+++ Assignments/Team Vikash A/Assignment 2/app.py
@@ -1,91 +1,70 @@
-from flask import Flask, render_template, request
-from markupsafe import escape
-
-import ibm_db
-conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=ea286ace-86c7-4d5b-8580-3fbfa46b1c66.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31505;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=xdb26131;PWD=yFenW4U3poX1JT3U",'','')
-
-app = Flask(__name__, template_folder="./Templates")
-
-
-
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+ 
+ 
+app = Flask(__name__)
+ 
+ 
+app.secret_key = 'your secret key'
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'your password'
+app.config['MYSQL_DB'] = 'WebLogin'
+ 
+mysql = MySQL(app)
+ 
 @app.route('/')
-def home():
-  return render_template('home_body.html')
-
-@app.route('/signin')
-def home():
-  return render_template('sign_in.html')
-
-@app.route('/signup')
-def home():
-  return render_template('sign_up.html')
-
-@app.route('/addstudent')
-def new_student():
-  return render_template('add_student.html')
-
-@app.route('/addrec',methods = ['POST', 'GET'])
-def addrec():
-  if request.method == 'POST':
-
-    name = request.form['name']
-    address = request.form['address']
-    city = request.form['city']
-    pin = request.form['pin']
-
-    sql = "SELECT * FROM students WHERE name =?"
-    stmt = ibm_db.prepare(conn, sql)
-    ibm_db.bind_param(stmt,1,name)
-    ibm_db.execute(stmt)
-    account = ibm_db.fetch_assoc(stmt)
-
-    if account:
-      return render_template('list.html', msg="You are already a member, please login using your details")
-    else:
-      insert_sql = "INSERT INTO students VALUES (?,?,?,?)"
-      prep_stmt = ibm_db.prepare(conn, insert_sql)
-      ibm_db.bind_param(prep_stmt, 1, name)
-      ibm_db.bind_param(prep_stmt, 2, address)
-      ibm_db.bind_param(prep_stmt, 3, city)
-      ibm_db.bind_param(prep_stmt, 4, pin)
-      ibm_db.execute(prep_stmt)
-    
-    return render_template('home.html', msg="Student Data saved successfuly..")
-
-@app.route('/list')
-def list():
-  students = []
-  sql = "SELECT * FROM Students"
-  stmt = ibm_db.exec_immediate(conn, sql)
-  dictionary = ibm_db.fetch_both(stmt)
-  while dictionary != False:
-    # print ("The Name is : ",  dictionary)
-    students.append(dictionary)
-    dictionary = ibm_db.fetch_both(stmt)
-
-  if students:
-     return render_template("list.html", students = students)
-
-@app.route('/delete/<name>')
-def delete(name):
-  sql = f"SELECT * FROM Students WHERE name='{escape(name)}'"
-  print(sql)
-  stmt = ibm_db.exec_immediate(conn, sql)
-  student = ibm_db.fetch_row(stmt)
-  print ("The Name is : ",  student)
-  if student:
-    sql = f"DELETE FROM Students WHERE name='{escape(name)}'"
-    print(sql)
-    stmt = ibm_db.exec_immediate(conn, sql)
-
-    students = []
-    sql = "SELECT * FROM Students"
-    stmt = ibm_db.exec_immediate(conn, sql)
-    dictionary = ibm_db.fetch_both(stmt)
-    while dictionary != False:
-      students.append(dictionary)
-      dictionary = ibm_db.fetch_both(stmt)
-    if students:
-      return render_template("list.html", students = students, msg="Delete successfully")
-
-  return "success..."
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in successfully !'
+            return render_template('index.html', msg = msg)
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+ 
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+ 
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password,rollno,email))
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg)
